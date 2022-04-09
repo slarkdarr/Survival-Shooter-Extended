7f781c04b791b4454f1fec9ffc56bba4173a65f8
ravmhmmd <13519201@std.stei.itb.ac.id>
2022-04-09 22:26:24 +0700

    early wavemode

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -8,6 +8,10 @@ public class EnemyManager : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject[] enemyPrefab;
 
+    public int waveWeightUsed = 0;
+    EnemyHealth enemyHealth;
+    public static int enemyKilled = 0;
+    public int enemySpawned = 0;
 
     void Start ()
     {
@@ -17,15 +21,48 @@ public class EnemyManager : MonoBehaviour
 
     void Spawn ()
     {
-        if (playerHealth.currentHealth <= 0f)
-        {
-            return;
+
+        if (GameLogics.mode == 1) {
+            if (playerHealth.currentHealth <= 0f)
+            {
+                return;
+            }
+
+
+            int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+            int spawnEnemy = Random.Range(0,enemyPrefab.Length);
+            Instantiate(enemyPrefab[spawnEnemy], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
         }
 
+        else if (GameLogics.mode == 2) {
 
-        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-        int spawnEnemy = Random.Range(0,enemyPrefab.Length);
-        Instantiate(enemyPrefab[spawnEnemy], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            int enemyWeight;
 
+            if (playerHealth.currentHealth <= 0f)
+            {
+                return;
+            }
+
+            while (waveWeightUsed < WaveManager.waveWeight) {
+                int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+                int spawnEnemy = Random.Range(0,enemyPrefab.Length);
+                enemy = enemyPrefab[spawnEnemy];
+                enemyHealth = enemy.GetComponent<EnemyHealth>();
+                enemyWeight = enemyHealth.weight;
+
+                if ((enemyWeight + waveWeightUsed) <= WaveManager.waveWeight) {
+                    Instantiate(enemyPrefab[spawnEnemy], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+                    waveWeightUsed += enemyWeight;
+                    enemySpawned += 1;
+                }
+            }
+
+            if (waveWeightUsed == WaveManager.waveWeight) {
+                if (enemyKilled == enemySpawned) {
+                    WaveManager.waveNum += 1;
+                    WaveManager.waveWeight += 5;
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class WaveManager : MonoBehaviour
+{
+    public static int waveNum;
+    public static int waveWeight;
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        waveNum = 1;
+        waveWeight = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level_01")) {
+
+            if (GameLogics.mode == 1) {
+                text.text = "";
+            }
+            else if (GameLogics.mode == 1) {
+                text.text = "Wave: " + waveNum;
+            }
+
+        }
+        
+    }
+}
